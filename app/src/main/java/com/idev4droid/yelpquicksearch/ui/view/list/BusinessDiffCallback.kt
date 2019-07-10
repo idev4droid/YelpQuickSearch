84ee7a06f00b606204c084ecec17910a18f313a2
msmith <matthieu.smith.cem@gmail.com>
2019-07-09 22:22:25 -0400

    Added kdoc

--- app/src/main/java/com/idev4droid/yelpquicksearch/ui/view/list/BusinessDiffCallback.kt
+++ app/src/main/java/com/idev4droid/yelpquicksearch/ui/view/list/BusinessDiffCallback.kt
@@ -3,6 +3,9 @@ package com.idev4droid.yelpquicksearch.ui.view.list
 import androidx.recyclerview.widget.DiffUtil
 import com.idev4droid.yelpquicksearch.core.data.model.Business
 
+/**
+ * BusinessDiffCallback is used to check for difference between business classes
+ */
 class BusinessDiffCallback : DiffUtil.ItemCallback<Business>() {
     override fun areItemsTheSame(oldItem: Business, newItem: Business): Boolean {
         return oldItem.id == newItem.id
